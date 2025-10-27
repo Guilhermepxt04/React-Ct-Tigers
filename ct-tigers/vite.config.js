b4d5faab8a7113693fa21b9dbf730f6400d3bfc8
Guilherme Peixoto <guilherme.pxt04@gmail.com>
2025-10-27 14:35:40 -0300

    feat: run build

---
+++ ct-tigers/vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap' 
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    sitemap({ 
+      hostname: 'https://kravmagatigers.com.br'
+    })
+  ],
+})
